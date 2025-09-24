ada9ddb147765b169c93f7f0eb6ccffc7a591541
Ahmed <7genial20177@gmail.com>
2025-09-24 03:27:49 +0300

    Update Experience.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -26,7 +26,7 @@ const Experience = () => {
       <div className="max-w-4xl mx-auto">
         <div className="text-center mb-16">
           <h2 className="text-4xl md:text-5xl font-bold mb-6 bg-accent-gradient bg-clip-text text-transparent">
-            الخبره
+           Professional Experience
           </h2>
           <p className="text-xl text-muted-foreground">
               رحلة من النمو والتعلم والمساهمات المؤثرة
